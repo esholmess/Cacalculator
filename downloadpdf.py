89015be442b559b9d09bf96227aa96a914c21b85
İlknur Yaren K. <163659184+esholmess@users.noreply.github.com>
2025-04-26 00:20:39 +0300

    Update downloadpdf.py

--- downloadpdf.py
+++ downloadpdf.py
@@ -3,8 +3,10 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+import textwrap  # fonksiyonun en üstüne eklersen yeterli
 
-# Türkçe karakter destekli fontu ekle
+
+
 pdfmetrics.registerFont(TTFont('DejaVu', 'DejaVuSans.ttf'))
 
 def save_as_pdf(results, category_footprints, recommendations, logo_path=None):
@@ -23,21 +25,27 @@ def save_as_pdf(results, category_footprints, recommendations, logo_path=None):
         y -= 20
 
     c.drawString(50, y, f"Toplam: {results['Toplam']:.2f} kg CO2")
-    y -= 20
+    y -= 30
     c.drawString(50, y, f"Kişi Başına: {results['Kisi Basi']:.2f} kg CO2")
-    y -= 40
+    y -= 30
+    c.drawString(50, y, f"Oda Başına {results['Oda Basi']:.2f} kg CO2")
+    y -= 30
+    c.drawString(50, y, f"Metrekare Başına: {results['Metrekare Basi']:.2f} kg CO2")
+    y -= 30
 
     c.setFont("DejaVu", 12)
     c.drawString(50, y, "Öneriler:")
     y -= 20
     c.setFont("DejaVu", 11)
     for rec in recommendations:
-        c.drawString(60, y, f"- {rec}")
-        y -= 15
-        if y < 50:
-            c.showPage()
-            c.setFont("DejaVu", 11)
-            y = height - 50
+        wrapped_lines = textwrap.wrap(rec, width=80)  # her satır max 90 karakter
+        for line in wrapped_lines:
+            c.drawString(60, y, line)
+            y -= 15
+            if y < 50:
+                c.showPage()
+                c.setFont("DejaVu", 11)
+                y = height - 50
 
     c.save()
     buffer.seek(0)
